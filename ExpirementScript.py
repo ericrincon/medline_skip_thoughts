6b3d9047b2a63f84bf11d2cc5f0e76d1068e0373
ericrincon <eric.rincon147@gmail.com>
2015-11-05 01:48:52 -0600

    Added new script

---
+++ ExpirementScript.py
@@ -0,0 +1,30 @@
+__author__ = 'ericrincon'
+
+from nltk.tokenize import sent_tokenize
+
+def preprocess_medline():
+    #Open file but read line by line.
+    #Where each line is an abstract.
+
+    processed_text = open('skip_thoughts_medline.txt', 'w')
+
+    with open('/work/03186/ericr/data/skip-thought/medline_titles_abstracts.txt') as file:
+        for line in file:
+            #Ignore the title of the medline abstract
+            abstract = line[line.find('.')+2:]
+
+            tokenized_abstract = sent_tokenize(abstract)
+
+            #Write abstracts to new file line by line in a contiguous text.
+            for sentence in tokenized_abstract:
+                processed_text.write(sentence)
+
+            #Add an end of document token
+            processed_text.write('<eod>.')
+
+
+def main():
+    preprocess_medline()
+
+if __name__ == '__main__':
+    main()
