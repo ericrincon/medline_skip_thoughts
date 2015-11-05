c1eddb76a22ac6ff3901249d7fdb2d4475d80da7
ericrincon <eric.rincon147@gmail.com>
2015-11-05 11:26:18 -0600

    Another fix

--- ExpirementScript.py
+++ ExpirementScript.py
@@ -11,13 +11,13 @@ def preprocess_medline():
     with open('/work/03186/ericr/data/skip-thought/medline_titles_abstracts.txt') as file:
         for line in file:
             #Ignore the title of the medline abstract
-            abstract = line[line.find('.')+2:].decode('utf-8')
+            abstract = line[line.find('.')+2:].encode('utf-8')
 
             tokenized_abstract = sent_tokenize(abstract)
 
             #Write abstracts to new file line by line in a contiguous text.
             for sentence in tokenized_abstract:
-                processed_text.write(sentence.encode('utf') + '\n')
+                processed_text.write(sentence.encode('utf-8') + '\n')
 
             #Add an end of document token
             processed_text.write('<eod>.\n')
