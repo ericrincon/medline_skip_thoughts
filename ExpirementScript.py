7919b73234227fbcafc4cb26a4eb5f5553b8b821
ericrincon <eric.rincon147@gmail.com>
2015-11-05 02:09:48 -0600

    Fixed unicode error

--- ExpirementScript.py
+++ ExpirementScript.py
@@ -6,14 +6,14 @@ def preprocess_medline():
     #Open file but read line by line.
     #Where each line is an abstract.
 
-    processed_text = open('skip_thoughts_medline.txt', 'w')
+    processed_text = open('skip_thoughts_medline.txt', 'a')
 
     with open('/work/03186/ericr/data/skip-thought/medline_titles_abstracts.txt') as file:
         for line in file:
             #Ignore the title of the medline abstract
             abstract = line[line.find('.')+2:]
 
-            tokenized_abstract = sent_tokenize(abstract)
+            tokenized_abstract = sent_tokenize(abstract.decode('utf-8'))
 
             #Write abstracts to new file line by line in a contiguous text.
             for sentence in tokenized_abstract:
@@ -28,3 +28,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    /home1/03186/ericr/code/skip_thought_research/medline_skip_thoughts
+    ExpirementScript.py
